2e1b7696850aa2bcb9f56520e036a33303b020ee
dannywaughman <dannyjameswaughman@msn.com>
2020-03-16 11:29:59 +0000

    Learning

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -7,7 +7,27 @@ import play.api.mvc._
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(<h1>Hello World!</h1>).as(HTML).withHeaders(
+      CACHE_CONTROL -> "max-age=3600",
+      EXPIRES -> "Expires: Wed, 21 Oct 2020 07:28:00 GMT"
+    ).withCookies(
+      Cookie("colour", "blue")
+    ).withSession("connected" -> "user@gmail.com"
+    )
+  }
+
+  def flash() = Action { implicit request =>
+    Ok{
+      request.flash.get("success").getOrElse("Welcome!")
+    }
+  }
+
+  def help() = Action {
+    Redirect("/flash").flashing("success" -> "You have been successfully redirected")
+  }
+
+  def infinite = Action {
+    Redirect("/infinite")
   }
 
 }
